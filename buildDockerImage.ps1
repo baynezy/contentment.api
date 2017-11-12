cf801ec6d5cdbc3ef4112b61598cc8f657bb01c4
Simon Baynes <baynezy@gmail.com>
2017-11-12 15:32:55 +0000

    Parking

--- buildDockerImage.ps1
+++ buildDockerImage.ps1
@@ -1,11 +1,14 @@
-param([string]$buildVersion, [string]$currentBranch, [string]$username, [string]$password)
+param([string]$buildVersion, [string]$currentBranch, [string]$username, [string]$secret)
 
 #if($currentBranch -eq 'master') {
 	Write-Host "building Docker image"
-	cd .\src\Contentment.Api
+	Set-Location .\src\Contentment.Api
 	docker build -t contentmentcms/contentment.api:$($buildVersion).beta .
 
 	Write-Host "pushing Docker image"
-	docker login -u="$($username)" -p="$($password)"
+	docker login -u="$($username)" -p="$($secret)"
 	docker push contentmentcms/contentment.api:$($buildVersion).beta
+
+	# reset location
+	Set-Location ../..
 #}
